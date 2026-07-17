823d0865a937dc1255ed1325619cdcee32180601
Avishi Aggarwal <avishi28810@gmail.com>
2026-07-17 14:22:57 +0530

    created ex1.js

---
+++ ex1.js
@@ -0,0 +1,14 @@
+// let a=10;
+// const b=25;
+// console.log("a=",a);
+// console.log("b=",b)
+// b=a++;
+// console.log("b=",b);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log("sum=" , sum(10,20));    
+const sum=(a,b)=>a+b;
+console.log("sum=" , sum(10,20));
+console.log("sum=" , sum(28,5));
